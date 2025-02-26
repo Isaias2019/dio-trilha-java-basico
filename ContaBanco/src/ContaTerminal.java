81707a68bd64b5a040c36e4aba5c378208b13ffe
isaias2019 <isaiassilva356@gmail.com>
2025-02-25 23:14:20 -0300

    primeiro commit

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Conta {
+    int numero;
+    String agencia;
+    String nomeCliente;
+    double saldo;
+}
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+       
+    }
+}
